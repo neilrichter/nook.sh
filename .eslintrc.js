1739eecba7da1f228abe98eaa36cf9f4fa54eed9
noook <me@neilrichter.com>
2020-04-11 20:51:34 +0200

    🚨 Set root alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,15 @@ module.exports = {
   rules: {
     'import/no-extraneous-dependencies': 'off',
     'nuxt/no-cjs-in-config': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', '.'],
+        ],
+        extensions: ['.ts', '.js', '.vue', '.json']
+      }
+    }
   }
 }
